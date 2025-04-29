e18d72a4b3003e48dcc4edaa9ff5487c19b56b16
ahmedelhefnawy <ahmedelhefnawy2003@yahoo.com>
2025-04-29 20:46:43 +0300

    Refactored data validation to utilize Pydantic models in FastAPI to enhance code clarity, maintainability, and validation consistency. Added feature of using the database to store interview instructions for every new Job Title and Interview Type, which saved the instructions generation time with each new interview. Removed the feature of storing interviews data in the database to prevent conflicts with service user data. Replaced storing interviews data with waiting for them after every chat request.

--- services/database.py
+++ services/database.py
@@ -1,33 +1,34 @@
 from sqlalchemy.orm import Session
-from models.schema import Interview
+from models.schema import Instructions
 
 class DatabaseService:
     def __init__(self, db: Session):
         self.db = db
-
-    def get_interview(self, interview_id: int) -> Interview:
-        """Fetch Interview Data from database using the interview ID"""
+    
+    def get_instructions(self, interview_type: str, job_title: str) -> str:
+        """Fetch Instructions from database using the instructions ID"""
         
-        return self.db.query(Interview).filter(Interview.interview_id == interview_id).first()
-
-    def add_interview(self, interview: Interview) -> None:
-        """Add interview to the database"""
-        self.db.add(interview)
+        return self.db.query(Instructions).filter(Instructions.interview_type == interview_type and Instructions.job_title == job_title).first()
+    
+    def add_instructions(self, instructions: Instructions) -> None:
+        """Add instructions to the database"""
+        
+        self.db.add(instructions)
         
         try:
             self.db.commit()
         except Exception as e:
-            print(f"Error adding interview: {e}")
+            print(f"Error adding instructions: {e}")
             self.db.rollback()
             raise e
+    
+    def del_instructions(self, instruction_id: int) -> None:
+        """Delete instructions from the database"""
         
-    def del_interview(self, interview_id: int) -> None:
-        """Delete interview from the database"""
+        instructions = self.db.query(Instructions).filter(Instructions.instruction_id == instruction_id).first()
         
-        interview = self.db.query(Interview).filter(Interview.interview_id == interview_id).first()
-        
-        if interview:
-            self.db.delete(interview)
+        if instructions:
+            self.db.delete(instructions)
             self.db.commit()
         else:
-            raise Exception(f"Interview with ID {interview_id} not found.")
+            raise Exception(f"Instructions with ID {instruction_id} not found.")
