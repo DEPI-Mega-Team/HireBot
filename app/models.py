bf903716e59a209045e306e4dd82ccfe05dd0a5f
ahmedelhefnawy <ahmedelhefnawy2003@yahoo.com>
2025-04-30 10:58:11 +0300

    Added api_key attribute to GeminiModel for explicity api_key providing

--- app/models.py
+++ app/models.py
@@ -5,14 +5,20 @@ from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmb
 load_dotenv()
 
 class GeminiModel:
-    def __init__(self):
+    def __init__(self, api_key=None):
+        self.api_key = api_key or os.getenv('GOOGLE_API_KEY')
+        if not self.api_key:
+            raise ValueError("API key is required.")
+
+        # Initialize the embedding and model
         self.embedding = GoogleGenerativeAIEmbeddings(
-            model='models/text-embedding-004', 
-            google_api_key=os.getenv('GOOGLE_API_KEY'),
+            model= 'models/text-embedding-004', 
+            google_api_key= self.api_key,
             )
         
+        # Initialize the model
         self.model = ChatGoogleGenerativeAI(
-            model='gemini-2.0-flash',
-            api_key=os.getenv('GOOGLE_API_KEY'),
+            model= 'gemini-2.0-flash',
+            api_key= self.api_key,
             temperature= 0.2,
         )
